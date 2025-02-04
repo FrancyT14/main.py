752b79df60f7df7540f0657c957f635a54ac10f6
FrancyT14 <turrinf14@gmail.com>
2025-02-04 20:11:14 +0100

    Update SAFE_PASSWORD.py
    Creatore di Password

--- SAFE_PASSWORD.py
+++ SAFE_PASSWORD.py
@@ -1,8 +1,17 @@
 import random
 
-password = ''
-caratteri = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-num_caratteri = int(input("Quanti caratteri vuoi nella tua password?(Scrivi un numero da 1 a 16)"))
-for i in range(num_caratteri):
-    password += random.choice(caratteri)
-print(password)
+def password():
+    while True:
+        password = ''
+        caratteri = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+        num_caratteri = int(input("Quanti caratteri vuoi nella tua password?(Scrivi un numero da 1 a 16)"))
+        for i in range(num_caratteri):
+            password += random.choice(caratteri)
+        domanda = True
+        print(password)
+        new_password = input("Ti piace questa password?(scrivi usando solo lettere maiuscole)")
+        if new_password == 'NO':
+            continue
+        elif new_password == 'SI':
+            print("La tua password e':", password)
+            break
